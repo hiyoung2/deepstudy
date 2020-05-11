7cc65707218e60114e9c3a4d72c680629b8edc24
young2e <pnp3737@gmail.com>
2020-05-12 00:52:48 +0900

    install

---
+++ test0001.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import keras
+
+# DLL load failed: 지정된 모듈을 찾을 수 없습니다. - 문제 발생
+# pip install "tensorflos < 2.1"로 해결
+
+print("tensorflow : ", tf.__version__) 
+
+print("keras : ", keras.__version__)
