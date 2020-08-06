e29a76952dc937c70109d311e94e2f6c2da85ac5
unknown <pnp3737@gmail.com>
2020-08-06 23:55:53 +0900

    0806_xml_to_txt

--- project_code/xml_to_yolotxt.py
+++ project_code/xml_to_yolotxt.py
@@ -1,9 +1,12 @@
+# labeling 정보가 담긴 xml 파일을 txt 형식으로 바꿈
+# from KimKiTae
+
 import glob, os, shutil
 from xml.etree.ElementTree import parse
  
-labels = ['kicking', 'punching', 'pushing']
-labelpath = 'C:/darknet-master/darknet-master/build/darknet/x64/mydata/annotations/'
-imgpath = 'C:/darknet-master/darknet-master/build/darknet/x64/mydata/images/'
+labels = ['kicking', 'punching', 'pushing'] # 작업했던 라벨링 이름(classes)
+labelpath = 'C:/darknet-master/darknet-master/build/darknet/x64/mydata/annotations/' # xml path
+imgpath = 'C:/darknet-master/darknet-master/build/darknet/x64/mydata/images/' # image path
  
 def file_path_save(): 
     filenames = [] 
@@ -35,4 +38,9 @@ def file_path_save():
     #         f.write(val[i] + "\n") 
  
 if __name__ == '__main__': file_path_save()
+
+# labeling 정보(bounding box 좌표값)가 xml과 txt 다른 방식으로 표현됨
+# xml은 xmax, xmin, ymax, ymin 이런 식으로 나타나는데
+# txt은 클래스인덱스값과 함께 비율로 나타난다
+
  
