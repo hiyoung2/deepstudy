dc7b04005de5590a6388a6107db7bd0edd2ed007
unknown <pnp3737@gmail.com>
2020-09-18 14:59:04 +0900

    0918

--- 0917/project.py
+++ 0917/project.py
@@ -5,14 +5,12 @@ import tensorflow as tf
 import numpy as np
 from keras.optimizers import Adam, RMSprop, SGD, Adadelta, Adagrad, Nadam
 from keras.layers import LeakyReLU
-from keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
+#from imblearn.pipeline import Pipeline
 from sklearn.model_selection import RandomizedSearchCV
 import dill as pickle
 from keras.utils import plot_model
-from imblearn.over_sampling import SMOTE
 
 def load_train(train_path, image_size, classes):
     images = []
@@ -34,7 +32,7 @@ def load_train(train_path, image_size, classes):
             #cv2.destroyAllWindows() 
             image = image.astype(np.float32)
             image = np.multiply(image, 1.0 / 255.0)
-            image1 = image.reshape(224,224)
+            image1 = image.reshape(224,224,1)
             images.append(image1)
             label = np.zeros(len(classes))
             label[index] = 1.0
@@ -62,42 +60,7 @@ x = result[0]
 y = result[1]
 
 
-'''
-x = np.array(result[0])
-y = np.array(result[1])
 
-#1-2. save to numpy
-np.save('D:/project/data/x_data.npy', x)
-np.save('D:/project/data/y_data.npy', y)
-
-x = np.load('D:/project/data/x_data.npy')
-y = np.load('D:/project/data/y_data.npy')
-'''
-
-#2. cnn network modeling
-def build_model(drop=0.1, optimizer='adam', learning_rate=0.1, activation='relu'):
-    inputs = tf.keras.Input(shape=(224,224), name='inputs')
-    x = tf.keras.layers.Conv1D(32, kernel_size=(3, 3), padding='same', activation=activation)(inputs)
-    x = tf.keras.layers.Dropout(drop)
-    x = tf.keras.layers.MaxPooling2D(pool_size=(2,2), strides=2)(x)
-    x = tf.keras.layers.Conv1D(64, kernel_size=(3,3), padding='same', activation=activation)(x)
-    x = tf.keras.layers.Dropout(drop)
-    x = tf.keras.layers.MaxPooling2D(pool_size=(2,2), strides=2)(x)
-    x = tf.keras.layers.Conv1D(128, kernel_size=(3,3), padding='same', activation=activation)(x)
-    x = tf.keras.layers.Conv1D(256, kernel_size=(3,3), padding='same', activation=activation)(x)
-    x = tf.keras.layers.Dropout(drop)
-    x = tf.keras.layers.Flatten()(x)
-    x = tf.keras.layers.Dense(16, padding='same')(x)
-    outputs = tf.keras.layers.Dense(2, activation='sigmoid')(x)
-    model = tf.keras.Model(inputs=inputs, outputs=outputs)
-    opt = optimizer(learning_rate=learning_rate)
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-    return model
-
-model = tf.keras.wrappers.scikit_learn.KerasClassifier(build_fn=build_model, verbose=1)
-
-#2-1. set pipeline
-pipe = Pipeline([("scaler", StandardScaler()), ("model", model)])
 
 #1-3. train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)#, validation_split=0.2)
@@ -107,6 +70,37 @@ print(f"x_train.shape: {x_train.shape}, x_test.shape: {x_test.shape}")
 print(f"y_train.shape: {y_train.shape}, y_test.shape: {y_test.shape}")
 # y_train.shape: (1600, 2), y_test.shape: (400, 2)
 
+
+#2. cnn network modeling
+def build_model(drop=0.1, optimizer='adam', learning_rate=0.1, activation='relu', kernel_size=(2,2)):
+    inputs = tf.keras.Input(shape=(224,224,1), name='inputs')
+    # x = tf.keras.layers.Reshape((1,224))(inputs)
+    x = tf.keras.layers.Conv2D(32, kernel_size=kernel_size, padding="same", activation=activation)(inputs)
+    x = tf.keras.layers.Dropout(drop)(x)
+    x = tf.keras.layers.MaxPooling2D(pool_size=(2,2), strides=2)(x)
+    x = tf.keras.layers.Conv2D(64, kernel_size=kernel_size, padding="same", activation=activation)(x)
+    x = tf.keras.layers.Dropout(drop)(x)
+    x = tf.keras.layers.MaxPooling2D(pool_size=kernel_size, strides=2)(x)
+    x = tf.keras.layers.Conv2D(128, kernel_size=kernel_size, padding="same", activation=activation)(x)
+    x = tf.keras.layers.Conv2D(256, kernel_size=kernel_size, padding="same", activation=activation)(x)
+    x = tf.keras.layers.Dropout(drop)(x)
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Dense(16)(x)
+    outputs = tf.keras.layers.Dense(2, activation='sigmoid')(x)
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+    opt = optimizer(learning_rate=learning_rate)
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+    return model
+
+model = tf.keras.wrappers.scikit_learn.KerasClassifier(build_fn=build_model, verbose=1)
+plot_model(model)
+
+#2-1. set pipeline
+# pipe = Pipeline([("scaler", StandardScaler()), ("model", model)])
+#pipe = Pipeline([("scaler", StandardScaler()), ('oversample', SMOTE(random_state=12)), ("model", model)])
+
+
+
 #x_train = x_train.reshape(-1*x_train.shape[1],x_train.shape[2])
 #y_train = y_train.reshape(y_train.shape[0]*y_train.shape[1],y_train.shape[2])
 
@@ -115,19 +109,24 @@ def create_hyperparameters():
     
     batches = [8,16,24,32,40]
     optimizers = [RMSprop, Adam, Adadelta, SGD, Adagrad, Nadam]
-    learning_rate = np.linspace(0.1,0.5,5).tolist()
+    learning_rate = [1e-5, 1e-4, 1e-3, 1e-2]
     dropout = np.linspace(0.1,0.5,5).tolist()
-    activation = ['tanh', 'relu', 'elu', "selu", "softmax", "sigmoid"]#, LeakyReLU()]
+    activation = ['tanh', 'relu', 'elu', "selu", "softmax", "sigmoid", LeakyReLU()]
+    kernel_size = [(2,2),(3,3)]
 
     # return: {key, value}
-    return{"model__batch_size": batches, "model__optimizer":optimizers,
+    return{"batch_size": batches, "optimizer":optimizers,
+           "learning_rate": learning_rate, "drop": dropout, 
+           "activation": activation, "kernel_size": kernel_size}
+
+    '''return{"model__batch_size": batches, "model__optimizer":optimizers,
            "model__learning_rate": learning_rate, "model__drop": dropout, 
-           "model__activation": activation}
+           "model__activation": activation}'''
 
 hyperparams = create_hyperparameters()
 
 #2-3. set RandomizedSearchCV
-search = RandomizedSearchCV(pipe, hyperparams, cv=3)#, n_jobs=-1)
+search = RandomizedSearchCV(model, hyperparams, cv=3)#, n_jobs=-1)
 # params = search.get_params().keys()
 # print(f"params: {params}")
 
@@ -143,11 +142,13 @@ search.fit(x_train, y_train)
 # 'pre_dispatch', 'random_state', 'refit', 'return_train_score', 'scoring', 'verbose'])`.
 # error solving..
 
-for param in search.get_params().keys():
-    print(f"param:\n{param}")
+#for param in search.get_params().keys():
+    #print(f"param:\n{param}")
 
-print(f"best_params of cnn network:\n {search.best__params__}")
-# {'model__optimizer': 'rmsprop', 'model__drop': 0.2, 'model__batch_size': 128}
+print(f"best_params of cnn network:\n {search.best_params_}")
+# best_params of cnn network:
+# {'optimizer': <class 'keras.optimizers.Adagrad'>, 'learning_rate': 0.0012000000000000001, 'kernel_size': (2, 2), 'drop': 0.1, 'batch_size': 32, 'activation': 'relu'}
+
 acc = search.score(x_test, y_test)
 print(f"acc: {acc}")
 # acc:  0.699999988079071
