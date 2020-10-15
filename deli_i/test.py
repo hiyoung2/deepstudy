0fb3bdeb32d95cc1822a24e2476cc31e34f3dc16
hainyoung <pnp3737@gmail.com>
2020-10-15 21:44:32 +0900

    1015

---
+++ deli_i/test.py
@@ -0,0 +1,11 @@
+# gathering data -> have to find the best recording condition
+# video data roi detect : date, time, speed, lane, auto
+# convert data to table
+# analysis video data 
+# if lane and auto -> off -> event! -> analysis causes of the event
+
+# python
+# opencv(binarization, hsv, pytesseract)
+
+# issues
+# : recording condition(especially tunnel), missing values, frames, lane sign
